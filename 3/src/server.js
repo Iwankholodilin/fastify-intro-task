e8bd1085faeab71d975921a2b83d91a3af8a997d
Iwankholodilin <iwan.kholodilin@yandex.ru>
2026-01-09 14:19:44 +0300

    Update server.js

--- 3/src/server.js
+++ 3/src/server.js
@@ -8,7 +8,16 @@ export default () => {
   const users = getUsers();
 
   // BEGIN (write your solution here)
-
+  app.get('/users', async (request, reply) => {
+    const page = parseInt(request.query.page) || 1;
+    const per = parseInt(request.query.per) || 5;
+    
+    const offset = (page - 1) * per;
+    
+    const paginatedUsers = users.slice(offset, offset + per);
+    
+    return paginatedUsers;
+  });
   // END
 
   return app;
