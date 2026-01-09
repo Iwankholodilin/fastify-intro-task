2ba4495069faaf5e55349713eb7883fe27ac90e4
Iwankholodilin <iwan.kholodilin@yandex.ru>
2026-01-09 14:20:24 +0300

    Update server.js

--- 4/src/server.js
+++ 4/src/server.js
@@ -7,7 +7,17 @@ export default () => {
   const companies = getCompanies();
 
   // BEGIN (write your solution here)
-
+  app.get('/companies/:id', async (request, reply) => {
+    const { id } = request.params;
+    
+    const company = companies.find(c => c.id === id);
+    
+    if (!company) {
+      return reply.code(404).send('Company not found');
+    }
+    
+    return company;
+  });
   // END
 
   return app;
