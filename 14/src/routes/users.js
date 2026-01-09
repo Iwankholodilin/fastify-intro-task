471a0bdef82e3103912aa9a4778ae1bc9ef69325
Iwankholodilin <iwan.kholodilin@yandex.ru>
2026-01-09 14:25:56 +0300

    Update users.js

--- 14/src/routes/users.js
+++ 14/src/routes/users.js
@@ -8,6 +8,35 @@ export default (app) => {
   app.get("/users/new", (req, res) => res.view("src/views/users/new"));
 
   // BEGIN (write your solution here)
-
+   app.post("/users", (req, res) => {
+    const { email, firstName, lastName } = req.body;
+    
+    const newUser = {
+      id: generateId(),
+      email,
+      firstName,
+      lastName,
+      token: generateToken(),
+    };
+    
+    users.push(newUser);
+    
+    res.setCookie('token', newUser.token);
+    
+    return res.redirect(`/users/${newUser.id}`);
+  });
+  
+  app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = users.find(u => u.id === id);
+    
+    const token = req.cookies?.token;
+    
+    if (!user || user.token !== token) {
+      return res.code(404).send('User not found');
+    }
+    
+    return res.view("src/views/users/show", { user });
+  });
   // END
 };
